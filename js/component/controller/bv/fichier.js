aca24f1711e5288c7d44b7661354e8fa4a84756f
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2017-05-26 15:27:00 +0200

    commercial tabs

--- js/component/controller/bv/fichier.js
+++ js/component/controller/bv/fichier.js
@@ -14,9 +14,6 @@ angular.module("components")
                     [65, 59, 80, 81, 56, 55, 40],
                     [28, 48, 40, 19, 86, 27, 90]
                 ];
-
-                console.log(this.data);
-
             }
         }
     });
--- js/component/controller/cercle/cercle.js
+++ js/component/controller/cercle/cercle.js
@@ -6,7 +6,18 @@ angular.module("components")
         templateUrl: './js/component/controller/cercle/cercle.html',
 
         controller: function () {
-            this.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-            this.data = [300, 500, 100];
+
+            this.getNumbers = () => {
+                let table = [];
+                table[0] = Math.random();
+                table[1] = 1 - table[0];
+
+                return table;
+            }
+
+            this.labels = ["Score", "Reste"];
+            this.data1 = this.getNumbers();
+            this.data2 = this.getNumbers();
+            this.data3 = this.getNumbers();
         }
     });
